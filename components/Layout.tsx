db03e265afc6228abb5db9c90ad4e2410e4cb978
Yash Gupta <yashguptaz@protonmail.com>
2021-09-18 19:04:03 +0530

    chore: Remove Nav

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,24 +1,28 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
-import Nav from "../components/Nav";
+//import Nav from "./Nav";
+import { styled } from "../stitches.config";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
+const LayoutCont = styled("div", {
+  background: "url('/bg.jpg') no-repeat ",
+  backgroundSize: "cover",
+});
+
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
+  <LayoutCont>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <Nav />
-    </header>
+    <header>{/* <Nav /> */}</header>
     {children}
-  </div>
+  </LayoutCont>
 );
 
 export default Layout;
