01de7e1ee2761fcd523a3dbd7de717fee995d4f8
Yash Gupta <yashguptaz@protonmail.com>
2021-09-18 18:33:40 +0530

    feat: Show check mark on copying email

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,4 +1,6 @@
 import { copyToClipboard } from "copy-lite";
+import React from "react";
+import ClipboardSvg from "../assets/CheckMark";
 import CopyIcon from "../assets/icons/copy";
 import { styled } from "../stitches.config";
 
@@ -29,13 +31,32 @@ const Cont = styled("div", {
     "-webkit-background-clip": "text",
     "-webkit-text-fill-color": "transparent",
   },
+  ".cyan:hover": {
+    background: "linear-gradient(180deg, #43F088 0%, #0db2db 150%)",
+    "-webkit-background-clip": "text",
+    "-webkit-text-fill-color": "transparent",
+  },
 });
 
-const copyEmail = () => {
-  copyToClipboard("yashguptaofficial@gmail.com");
-};
-
 const Connect = () => {
+  const [copied, setCopied] = React.useState<boolean>(false);
+  let copyTimer: number | null | NodeJS.Timeout = null;
+
+  React.useEffect(() => {
+    () => {
+      clearTimeout(copyTimer as number);
+    };
+  });
+
+  const copyEmail = () => {
+    copyToClipboard("yashguptaofficial@gmail.com");
+    setCopied(true);
+
+    copyTimer = setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
     <Cont id="connect">
       <h2>Connect</h2>
@@ -43,11 +64,14 @@ const Connect = () => {
       <div className="copyEmail">
         <span>yashguptaofficial@gmail.com</span>
         <span data-tooltip="Click to Copy">
-          <CopyIcon onClick={copyEmail} />
+          {copied ? <ClipboardSvg /> : <CopyIcon onClick={copyEmail} />}
         </span>
       </div>
       <p>
-        Or you can DM me on <span className="cyan">Twitter</span>
+        Or you can DM me on{" "}
+        <a href="https://twitter.com/yashguptaz" target="_blank">
+          <span className="cyan">Twitter</span>
+        </a>
       </p>
     </Cont>
   );
