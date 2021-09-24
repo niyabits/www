1c01dd7d61441670019f18b2c92a205e73516988
Yash Gupta <yashguptaz@protonmail.com>
2021-09-24 15:03:56 +0530

    chore: add icons

---
+++ assets/icons/ExtLink.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+function ExtLink(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-external-link"
+      {...props}
+    >
+      <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6m4-3h6v6m-11 5L21 3" />
+    </svg>
+  );
+}
+
+export default ExtLink;
